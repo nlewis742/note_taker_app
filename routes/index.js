49bcb13e5653058f28e7df73e436717f73c84422
Natasha <nlackas12@gmail.com>
2023-05-05 14:22:38 -0500

    fixing comments

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-// All of these routes are prepended with '/api'
+
+
+// this is our READ route
 router.get("/notes", (req, res) => {
   // We first need to bring in the data from the db.json file
   // We can do this by using the fs module
@@ -14,14 +16,14 @@ router.get("/notes", (req, res) => {
     // We need to send the data back to the client
     res.json(notes);
   });
-  // We need to read the file and then parse the data
+  
 });
 
 // this our CREATE route
 router.post("/notes", (req, res) => {
   console.log("Req Body: ", req.body);
   console.log("Req Body Type: ", typeof req.body);
-  // First I would check that we got the data from the FORM(client)
+
   // Here we create a new note object
   const newNote = {
     id: uuidv4(),
@@ -31,12 +33,7 @@ router.post("/notes", (req, res) => {
   // We need to read the file and then parse the data
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
-    console.log("Data: ", data);
-    console.log("Type: ", typeof data); // string
-    // We need to parse the data
-    let notes = JSON.parse(data); // object --> array
-    console.log("Notes: ", notes);
-    console.log("Type: ", typeof notes); // object
+     let notes = JSON.parse(data); 
 
     // We need to add the new note to the array
     notes.push(newNote);
@@ -52,7 +49,7 @@ router.post("/notes", (req, res) => {
 });
 
 router.delete("/notes/:id", (req, res) => {
-  console.log("Req Params: ", req.params); // { id: 'kjhsdf0-kjhsdfkjhse-w9872341' }
+  console.log("Req Params: ", req.params); 
 
   // We need to read the file and then parse the data
   fs.readFile("./db/db.json", "utf8", (err, data) => {
@@ -60,12 +57,7 @@ router.delete("/notes/:id", (req, res) => {
     // We need to parse the data
     let notes = JSON.parse(data); // object --> array
 
-    // We need to filter the notes array
-    //     Array.forEach(function(iterator) )
-    //    Array.map(iterator => {
-    //       return iterator * 2;
-    //   })
-
+    // We need to filter out the note that we want to delete
     let filteredNotes = notes.filter((note) => {
       return note.id !== req.params.id;
     });
--- server.js
+++ server.js
@@ -21,15 +21,11 @@ app.use('/api', api);
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
+// GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// // GET Route for feedback page
-// app.get('/feedback', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-// );
 
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
